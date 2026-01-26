554d73d36165376855eb6d096ccbf3c7706e5f62
aditya5759 <aditya5759098@gmail.com>
2026-01-27 00:27:46 +0530

    [Aditya] Class Work

---
+++ IfElseExample.java
@@ -0,0 +1,16 @@
+//Example for if statement
+
+class IfElseExample{
+public static void main(String args[])  // main method
+{
+
+ int age=20;
+ 
+ if(age >= 18)
+ {
+     
+     System.out.println("Eligible" );  //
+
+}
+}
+}
