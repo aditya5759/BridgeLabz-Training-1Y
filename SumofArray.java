744dda60ac3235b65feaacaab51cc18b004cda2e
aditya5759 <aditya5759098@gmail.com>
2026-02-03 09:14:22 +0530

    3rd Feb

---
+++ SumofArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class SumofArray{
+	public static void main (String[]args){
+		int[] arr=new int[5];
+		int s=0;
+		Scanner sc=new Scanner (System.in);
+		System.out.print("Enter 5 elements of the array");
+		for(int i=0; i<5; i++)
+		{
+		arr[i]=sc.nextInt();}
+		for(int i=0; i<5; i++)
+		{
+			s+=arr[i];
+		}
+		System.out.println("Sum of all the elements of the array is "+s);
+	}
+}
